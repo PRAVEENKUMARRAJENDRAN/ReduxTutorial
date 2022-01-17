932392c726c6ebebe6514ab630cf05ed1a91eada
PRAVEENKUMARRAJENDRAN <praveenrajendran2109@gmail.com>
2022-01-17 14:09:59 -0500

    "Last Commit"

---
+++ ReduxTutorial/src/redux/actions/productActions.js
@@ -0,0 +1,23 @@
+import { actionTypes } from "../constants/actionTypes";
+
+
+export const setProducts = (products) => {
+    return {
+        type: actionTypes.SET_PRODUCT,
+        payload: products
+    }
+}
+
+
+export const selectedProduct = (product) => {
+    return {
+        type: actionTypes.SELECTED_PRODUCT,
+        payload: product
+    }
+}
+
+export const removeSelectedProduct = () => {
+    return {
+        type: actionTypes.REMOVE_SELECTED_PRODUCT
+    }
+}
